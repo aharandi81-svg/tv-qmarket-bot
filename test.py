70d071f9b4edbc7ae82319e1a0fcb34059da1ce7
aharandi81-svg <a.harandi.81@gmail.com>
2026-06-30 19:55:13 +0330

    Update test.py

--- test.py
+++ test.py
@@ -2,17 +2,23 @@ from playwright.sync_api import sync_playwright
 
 URL = "https://www.tradingview.com/markets/cryptocurrencies/prices-all/"
 
-
 with sync_playwright() as p:
 
     browser = p.chromium.launch(headless=True)
 
-    page = browser.new_page()
+    page = browser.new_page(
+        viewport={"width": 1920, "height": 1080}
+    )
 
     page.goto(URL, wait_until="networkidle")
 
-    print(page.title())
+    page.wait_for_timeout(5000)
 
-    page.screenshot(path="page.png", full_page=True)
+    html = page.content()
+
+    with open("page.html", "w", encoding="utf-8") as f:
+        f.write(html)
+
+    print("HTML Saved")
 
     browser.close()
