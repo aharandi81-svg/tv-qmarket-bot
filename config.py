b361bb75b70ae6e80f2a7880ee49af8082e2b117
aharandi81-svg <a.harandi.81@gmail.com>
2026-06-30 19:31:14 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+SYMBOLS = {
+    "gold": {
+        "exchange": "OANDA",
+        "symbol": "XAUUSD",
+        "screener": "forex"
+    },
+    "btc": {
+        "exchange": "BINANCE",
+        "symbol": "BTCUSDT",
+        "screener": "crypto"
+    },
+    "bnb": {
+        "exchange": "BINANCE",
+        "symbol": "BNBUSDT",
+        "screener": "crypto"
+    }
+}
